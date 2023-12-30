9ce8296033ca4db447dda1e10472344ed15d53d2
Emika <khaw.rkk@gmail.com>
2023-12-30 02:10:19 -0800

    display messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +9,23 @@ import { HeroService } from '../hero.service';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void {
-  this.heroService.getHeroes()
+    this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-}
+  }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
